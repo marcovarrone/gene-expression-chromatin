a14f12fa94e5b8bb13221e9cb3056b8d9709a20c
Marco Varrone <mrc.varrone@gmail.com>
2019-05-28 10:03:53 +0200

    Fix autoencoder dropout & add regularization

--- src/embedding/autoencoder.py
+++ src/embedding/autoencoder.py
@@ -11,10 +11,10 @@ from tensorflow.python.keras.callbacks import TensorBoard
 class Autoencoder(object):
 
     def __init__(self, in_dim, embedding_size, encoder_sizes=None, decoder_sizes=None, learning_rate=0.001,
-                 activation=tf.nn.relu,
-                 loss=keras.metrics.mean_squared_error, optimizer=keras.optimizers.Adam, batch_norm=False,
-                 dropout_in=None, dropout=None, max_norm=None, initializer='glorot_uniform',
-                 patience=10, checkpoint_every=0, save_model=False, run_folder=None, offset=0):
+                 activation=tf.nn.relu, regularizer=None, loss=keras.metrics.mean_squared_error,
+                 optimizer=keras.optimizers.Adam, batch_norm=False, dropout_in=0.0, dropout=0.0, max_norm=None,
+                 initializer='glorot_uniform', patience=10, checkpoint_every=0, save_model=False, run_folder=None,
+                 offset=0):
         super().__init__()
         self.in_dim = in_dim
         self.embedding_size = embedding_size
@@ -29,6 +29,7 @@ class Autoencoder(object):
         self.learning_rate = learning_rate
         self.batch_size = None
         self.activation = activation
+        self.regularizer = regularizer
         self.loss = loss
         self.optimizer = optimizer
         self.batch_norm = batch_norm
@@ -52,21 +53,23 @@ class Autoencoder(object):
         input_data = Input(shape=(self.in_dim,))
         x = input_data
 
-        for i, encoder_size in enumerate(self.encoder_sizes):
-            x = Dense(units=encoder_size, activation=self.activation, kernel_initializer=self.initializer)(x)
-            if self.dropout:
-                x = Dropout(self.dropout)(x)
+        x = Dropout(self.dropout_in)(x)
 
-        x = Dense(units=self.embedding_size, activation=self.activation, kernel_initializer=self.initializer)(x)
-        if self.dropout:
+        for i, encoder_size in enumerate(self.encoder_sizes):
+            x = Dense(units=encoder_size, activation=self.activation, kernel_initializer=self.initializer,
+                      activity_regularizer=self.regularizer, bias_regularizer=self.regularizer)(x)
             x = Dropout(self.dropout)(x)
+
+        x = Dense(units=self.embedding_size, activation=self.activation, kernel_initializer=self.initializer,
+                  kernel_regularizer=self.regularizer, bias_regularizer=self.regularizer)(x)
+        #x = Dropout(self.dropout)(x)
 
         self.encoder = Model(inputs=input_data, outputs=x)
 
         for decoder_size in self.decoder_sizes:
-            x = Dense(units=decoder_size, activation=self.activation, kernel_initializer=self.initializer)(x)
-            if self.dropout:
-                x = Dropout(self.dropout)(x)
+            x = Dense(units=decoder_size, activation=self.activation, kernel_initializer=self.initializer,
+                      activity_regularizer=self.regularizer, bias_regularizer=self.regularizer)(x)
+            x = Dropout(self.dropout)(x)
 
         x = Dense(units=self.in_dim)(x)
         self.model = Model(inputs=input_data, outputs=x)
@@ -78,11 +81,13 @@ class Autoencoder(object):
             steps_per_epoch=None, validation_steps=None, force_train=False, **kwargs):
         self.batch_size = batch_size
 
+
+
         if os.path.isfile('models/' + str(self) + '_weights.h5') and not force_train:
-            print("This model has already been trained. Loading from file")
+            print("The model "+str(self)+" has already been trained. Loading from file")
             self.encoder.load_weights('models/' + str(self) + '_weights.h5')
             return
-
+        print("Start training of model "+str(self))
         if callbacks is None:
             callbacks = []
         if not isinstance(callbacks, list):
@@ -122,6 +127,7 @@ class Autoencoder(object):
         name = 'autoencoder'
         for encoder in self.encoder_sizes:
             name += '_' + str(encoder)
+        name += '_' + str(self.embedding_size)
 
         for decoder in self.decoder_sizes:
             name += '_' + str(decoder)
@@ -132,6 +138,9 @@ class Autoencoder(object):
             name += '_dri' + str(self.dropout_in)
         if self.dropout:
             name += '_dr' + str(self.dropout)
+        if self.regularizer:
+            name += '_l1_' + str(self.regularizer.l1)
+            name += '_l2_' + str(self.regularizer.l2)
         if self.max_norm:
             name += '_max' + str(self.max_norm)
         if self.initializer != 'glorot_uniform':

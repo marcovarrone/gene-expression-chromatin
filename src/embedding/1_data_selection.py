66a29f3e3fb1b39230702be03fb9f4f8b05b4e7d
Marco Varrone <mrc.varrone@gmail.com>
2019-07-14 18:03:12 +0200

    Add shuffling in data selection & miscs

--- src/embedding/1_data_selection.py
+++ src/embedding/1_data_selection.py
@@ -15,14 +15,15 @@ parser = argparse.ArgumentParser()
 
 #ToDo: add description
 parser.add_argument('n_samples', type=int)
-parser.add_argument('offset', type=int, nargs='?', default=0)
-parser.add_argument('normalize', type=bool, nargs='?', default=True)
-parser.add_argument('valid_size', type=float, nargs='?', default=0.2)
+parser.add_argument('--offset', type=int, nargs='?', default=0)
+parser.add_argument('--normalize', type=bool, nargs='?', default=True)
+parser.add_argument('--valid_size', type=float, nargs='?', default=0.2)
 
 args = parser.parse_args()
 
 print('Selecting samples from '+str(args.offset)+' to '+str(args.offset + args.n_samples))
 X = np.hstack((X_train, Y_train))
+np.random.shuffle(X)
 X = X[args.offset:args.offset + args.n_samples]
 X = X.T
 
@@ -42,11 +43,11 @@ if args.valid_size:
     representation += '_'+str(args.valid_size)
 
     print('Saving train set with size '+str(X_train.shape))
-    np.save(str(data_folder)+'/GSE92743_X_genes' + str(representation), X_train)
+    np.save(str(data_folder)+'/GSE92743_X_train_genes' + str(representation), X_train)
 
     print('Saving validation set with size ' + str(X_valid.shape))
-    np.save(str(data_folder)+'/GSE92743_y_genes' + str(representation), X_valid)
+    np.save(str(data_folder)+'/GSE92743_X_valid_genes' + str(representation), X_valid)
 else:
     print('Saving train set with size ' + str(X.shape))
-    np.save(str(data_folder)+'/GSE92743_X_genes'+str(representation), X)
+    np.save(str(data_folder)+'/GSE92743_X_train_genes'+str(representation), X)
 

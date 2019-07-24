7340dde20a1471253d57eb167bbe4f2b79fdc83d
Marco Varrone <mrc.varrone@gmail.com>
2019-07-24 15:39:57 +0200

    Data selection also for entire dataset instead of selecting samples

--- src/embedding/1_data_selection.py
+++ src/embedding/1_data_selection.py
@@ -1,8 +1,9 @@
 import argparse
 import configparser
+
 import numpy as np
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 config = configparser.ConfigParser()
 config.read('/home/varrone/config.ini')
@@ -13,8 +14,8 @@ data_folder = config['EMBEDDING']['DATA']
 
 parser = argparse.ArgumentParser()
 
-#ToDo: add description
-parser.add_argument('--n_samples', type=int, default=20000)
+# ToDo: add description
+parser.add_argument('--n_samples', type=int)
 parser.add_argument('--dataset', type=str, default='GSE92743')
 parser.add_argument('--offset', type=int, nargs='?', default=0)
 parser.add_argument('--no-normalize', default=False, action='store_true')
@@ -22,13 +23,23 @@ parser.add_argument('--valid-size', type=float, nargs='?', default=0)
 
 args = parser.parse_args()
 
-print('Selecting samples from '+str(args.offset)+' to '+str(args.offset + args.n_samples))
 X = np.hstack((X_train, Y_train))
 np.random.shuffle(X)
-X = X[args.offset:args.offset + args.n_samples]
-X = X.T
 
-representation = '_'+str(args.n_samples)+'_'+str(args.offset)
+representation = ''
+
+if args.n_samples:
+    representation += '_genes_' + str(args.n_samples) + '_' + str(args.offset)
+    print('Selecting samples from ' + str(args.offset) + ' to ' + str(args.offset + args.n_samples))
+
+    X = X[args.offset:args.offset + args.n_samples]
+    X = X.T
+
+    genes_dim = 0
+    samples_dim = 1
+else:
+    genes_dim = 1
+    samples_dim = 0
 
 if not args.no_normalize:
     print('Normalizing')
@@ -38,17 +49,16 @@ if not args.no_normalize:
     representation += '_normalized'
 
 if args.valid_size:
-    print('Assigning '+str(args.valid_size*100)+'% of genes to validation set')
+    print('Assigning ' + str(args.valid_size * 100) + '% of genes to validation set')
     X_train, X_valid = train_test_split(X, test_size=args.valid_size)
 
-    representation += '_'+str(args.valid_size)
+    representation += '_' + str(args.valid_size)
 
-    print('Saving training set with '+str(X_train.shape[0])+' genes and '+str(X_train.shape[1])+' samples')
-    np.save(str(data_folder)+'/'+str(args.dataset)+'/X_train_genes_' + str(representation), X_train)
+    print('Saving training set with ' + str(X_train.shape[genes_dim]) + ' genes and ' + str(X_train.shape[samples_dim]) + ' samples')
+    np.save(str(data_folder) + '/' + str(args.dataset) + '/X_train' + str(representation), X_train)
 
-    print('Saving validation set with '+str(X_valid.shape[0])+' genes and '+str(X_valid.shape[1])+' samples')
-    np.save(str(data_folder)+'/'+str(args.dataset)+'/X_valid_genes_' + str(representation), X_valid)
+    print('Saving validation set with ' + str(X_valid.shape[genes_dim]) + ' genes and ' + str(X_valid.shape[samples_dim]) + ' samples')
+    np.save(str(data_folder) + '/' + str(args.dataset) + '/X_valid' + str(representation), X_valid)
 else:
-    print('Saving training set with size '+str(X.shape[0])+' genes and '+str(X.shape[1])+' samples')
-    np.save(str(data_folder)+'/'+str(args.dataset)+'/X_train_genes_'+str(representation), X)
-
+    print('Saving training set with size ' + str(X.shape[genes_dim]) + ' genes and ' + str(X.shape[samples_dim]) + ' samples')
+    np.save(str(data_folder) + '/' + str(args.dataset) + '/X_train' + str(representation), X)

937de49f6cb1796955aa374dbeba370b0bc03a86
Marco Varrone <mrc.varrone@gmail.com>
2019-07-24 14:06:27 +0200

    Embedding plots

--- src/embedding/3_embedding_graphsage.py
+++ src/embedding/3_embedding_graphsage.py
@@ -2,11 +2,11 @@ import argparse
 import os
 
 import networkx as nx
-import pandas as pd
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-data-repr', '--data-representation', type=str, default='20000_0_normalized')
-parser.add_argument('-emb-repr', '--embedding-representation', type=str, default='autoencoder_50_e120_lr0.0001_bs128_bn')
+parser.add_argument('-emb-repr', '--embedding-representation', type=str,
+                    default='autoencoder_50_e50_lr0.0001_bs128_bn_20000_0_normalized')
 parser.add_argument('--dataset', type=str, default='GSE92743')
 parser.add_argument('--save-embedding', default=False, action='store_true')
 parser.add_argument('--random-seed', type=int, default=42)
@@ -16,7 +16,6 @@ parser.add_argument('--threshold', type=float, default=0.0001)
 parser.add_argument('--gpu', default=False, action='store_true')
 parser.add_argument('--save-model', default=False, action='store_true')
 
-
 args = parser.parse_args()
 
 if not args.gpu:
@@ -24,16 +23,15 @@ if not args.gpu:
     os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 import configparser
-import stellargraph as sg
 import scipy.sparse as sps
 import numpy as np
 import tensorflow as tf
-from stellargraph.data import EdgeSplitter
-from stellargraph.mapper import GraphSAGELinkGenerator, GraphSAGENodeGenerator
-from stellargraph.layer import GraphSAGE, link_classification
 from graphsage import GraphSAGELinkPredictor
 
-import keras
+config_tf = tf.compat.v1.ConfigProto(device_count={'GPU': 0, 'CPU': 10})
+config_tf.intra_op_parallelism_threads = 10
+config_tf.inter_op_parallelism_threads = 10
+tf.compat.v1.Session(config=config_tf)
 
 config = configparser.ConfigParser()
 config.read('/home/varrone/config.ini')
@@ -43,13 +41,18 @@ tf.set_random_seed(args.random_seed)
 
 adjacency = sps.load_npz(config['GRAPH']['GENEMANIA_NPZ']).todense()
 adjacency[adjacency < args.threshold] = 0
+adjacency[adjacency >= args.threshold] = 1
 
 g_nx = nx.from_numpy_matrix(adjacency)
 
 node_features = np.load('embeddings/' + str(args.dataset) + '/' + str(args.embedding_representation) + '.npy')
 
-graphsage = GraphSAGELinkPredictor(graph=g_nx, node_features=node_features, p_test=0.0, learning_rate=1e-5)
+# ToDo: parameters from command line
+graphsage = GraphSAGELinkPredictor(graph=g_nx, node_features=node_features, p_test=0.1, learning_rate=1e-4, epochs=12,
+                                   save_model=args.save_model)
 graphsage.fit()
+
+# ToDo: fix embedding name saving
 
 if args.save_embedding:
     embeddings = graphsage.embeddings

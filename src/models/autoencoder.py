3347c000dcba8775e07522752981b625f51d866d
Marco Varrone <mrc.varrone@gmail.com>
2019-08-03 19:46:37 +0200

    Autoencoder inheritance from ModelNN

--- src/models/autoencoder.py
+++ src/models/autoencoder.py
@@ -10,17 +10,18 @@ from keras.layers import Dense, Input, Dropout
 from keras.models import Model
 from tensorflow.python.keras.callbacks import TensorBoard
 
+from models.model import ModelNN
 
 # ToDo: optimize GPU memory usage
 
-class Autoencoder(object):
+class Autoencoder(ModelNN):
 
     def __init__(self, in_dim, embedding_size, encoder_sizes=None, decoder_sizes=None, learning_rate=0.001,
                  activation=tf.nn.relu, regularizer=None, loss=keras.metrics.mean_squared_error,
                  optimizer=keras.optimizers.Adam, batch_norm=False, dropout_in=0.0, dropout=0.0, max_norm=None,
                  initializer='glorot_uniform', patience=10, checkpoint_every=0, save_model=False, run_folder=None,
                  data_representation=''):
-        super().__init__()
+
         self.in_dim = in_dim
         self.embedding_size = embedding_size
         if encoder_sizes is None:
@@ -43,17 +44,11 @@ class Autoencoder(object):
         self.dropout = dropout
         self.max_norm = max_norm
         self.initializer = initializer
-        self.patience = patience
-        self.checkpoint_every = checkpoint_every
-        self.save_model = save_model
-        self.run_folder = run_folder
         self.data_repr = data_representation
 
-        self.history = None
         self.encoder = None
-        self.model = None
 
-        self._build_model()
+        super().__init__(patience, checkpoint_every, save_model, run_folder)
 
     def _build_model(self):
         input_data = Input(shape=(self.in_dim,))
@@ -72,63 +67,38 @@ class Autoencoder(object):
 
         self.encoder = Model(inputs=input_data, outputs=x)
 
+
         for decoder_size in self.decoder_sizes:
             x = Dense(units=decoder_size, activation=self.activation, kernel_initializer=self.initializer,
                       activity_regularizer=self.regularizer, bias_regularizer=self.regularizer)(x)
             x = Dropout(self.dropout)(x)
 
         x = Dense(units=self.in_dim)(x)
-        self.model = Model(inputs=input_data, outputs=x)
+        model = Model(inputs=input_data, outputs=x)
         optimizer = self.optimizer(lr=self.learning_rate)
-        self.model.compile(loss=self.loss, optimizer=optimizer, metrics=[self.loss])
+        model.compile(loss=self.loss, optimizer=optimizer, metrics=[self.loss])
+
+        self.output_model = self.encoder
+        return model
 
     def fit(self, x=None, batch_size=128, epochs=1, verbose=1, callbacks=None, validation_split=0.,
             validation_data=None, shuffle=True, class_weight=None, sample_weight=None, initial_epoch=0,
             steps_per_epoch=None, validation_steps=None, force_train=False, **kwargs):
         self.batch_size = batch_size
         self.epochs = epochs
-        if os.path.isfile('models/' + str(self) + '_weights.h5') and not force_train:
-            print("The model " + str(self) + " has already been trained. Loading from file")
-            self.encoder.load_weights('models/' + str(self) + '_weights.h5')
+
+        if not force_train and self._load_model():
             return
+
         print("Start training of model " + str(self))
-        if callbacks is None:
-            callbacks = []
-        if not isinstance(callbacks, list):
-            callbacks = [callbacks]
-
-        if self.patience > 0:
-            es = EarlyStopping(monitor='val_loss', verbose=1, patience=self.patience, restore_best_weights=True)
-            callbacks.append(es)
-
-        if self.checkpoint_every > 0:
-            mc = ModelCheckpoint('weights/' + str(self) + '.h5',
-                                 save_weights_only=False, period=self.checkpoint_every)
-            callbacks.append(mc)
-
-        if self.run_folder:
-            tb = TensorBoard(log_dir=str(self.run_folder) + '/' + str(self))
-            callbacks.append(tb)
+        callbacks = self._add_callbacks(callbacks)
 
         self.model.fit(x, x, batch_size, epochs, verbose, callbacks, validation_split,
                        validation_data, shuffle, class_weight, sample_weight,
                        initial_epoch, steps_per_epoch, validation_steps, **kwargs)
 
         if self.save_model:
-            if validation_data:
-                print("Warning: saving a model which has not been trained on all the genes.")
-
-            if os.path.isfile('models/' + str(self) + '_weights.h5'):
-                os.remove('models/' + str(self) + '_weights.h5')
-            self.encoder.save_weights('models/' + str(self) + '_weights.h5')
-
-    def evaluate(self, x=None, y=None, batch_size=None, verbose=1, sample_weight=None, steps=None):
-        return self.model.evaluate(x, y, batch_size, verbose, sample_weight, steps)
-
-    '''def load_encoder(self, batch_size):
-        self.batch_size = batch_size
-        # self.model = keras.models.load_model('models/'+str(self)+'_full.h5')
-        print("Loading model", str(self))'''
+            self._save_model(validation_data)
 
     def __str__(self):
         name = 'autoencoder'

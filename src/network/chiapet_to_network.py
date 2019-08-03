85b57e336aadffd7798c88119d3455b6b38dc742
Marco Varrone <mrc.varrone@gmail.com>
2019-08-03 19:46:48 +0200

    Add ModelNN

--- src/network/chiapet_to_network.py
+++ src/network/chiapet_to_network.py
@@ -10,7 +10,7 @@ def build_graph(A):
 
 def main():
     bin_length = 10000000
-    file_path = 'ENCSR000BZX_HCT116_POLR2A.bed'
+    file_path = '/home/varrone/gene-expression-chromatin/data/ENCSR000BZX_HCT116_POLR2A.bed'
 
     contact_matrix = ChiaPetInteractions(file_path, bin_length, different_chrs=False)
     contact_matrix.align_interactions()
@@ -19,7 +19,7 @@ def main():
     G = build_graph(heatmap)
     G.remove_nodes_from(list(nx.isolates(G)))
     pos = nx.spring_layout(G, k=0.5, iterations=20)
-    plt.figure(figsize=(20,20))
+    plt.figure(figsize=(10,10))
 
     nx.draw(G, pos, node_size=300, with_labels=False)
 

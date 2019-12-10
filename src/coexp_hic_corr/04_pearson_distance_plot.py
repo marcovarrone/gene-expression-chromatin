f2e0e61e78bf612ccbcb4cf1ee8cde2e9c840adb
Marco Varrone <mrc.varrone@gmail.com>
2019-12-10 13:35:38 +0100

    Normalize coexpression by real distance pt.1

--- src/coexp_hic_corr/04_pearson_distance_plot.py
+++ src/coexp_hic_corr/04_pearson_distance_plot.py
@@ -2,6 +2,9 @@ import argparse
 import numpy as np
 from math import sqrt
 import matplotlib.pyplot as plt
+import pandas as pd
+from scipy import stats
+import itertools
 
 def get_values_from_distance(matrix):
     values = []
@@ -12,48 +15,90 @@ def get_values_from_distance(matrix):
         distances.append(d)
     return distances, values
 
+def get_values_from_real_distance(dataset, chr, coexpression):
+    rna = pd.read_csv('/home/varrone/Prj/gene-expression-chromatin/src/coexp_hic_corr/data/{}/rna/{}_chr_{:02d}_rna.csv'.format(dataset, dataset, chr))
+    tss = rna['Transcription start site (TSS)']
+    #tss_binned = np.digitize(tss.to_numpy(), np.arange(0, np.max(tss)+50000, 50000))*50000
+    coexpression += 1
+    combinations = np.array(list(itertools.combinations(range(len(tss)), 2)))
+    distances = np.abs(tss[combinations[:, 1]].to_numpy() - tss[combinations[:, 0]].to_numpy())
+    values = coexpression[combinations[:, 0], combinations[:, 1]]
+    #for distance in np.unique(distances):
+    #    values_distance =
+
+    values, bin_edges, bin_number = stats.binned_statistic(distances, values, 'mean', bins=len(tss)//3)
+    coexpression_norm = coexpression.copy()
+
+    plt.imshow(coexpression, cmap='Oranges')
+    plt.show()
+
+    coexpression_norm[combinations[:, 0], combinations[:, 1]] /= values[bin_number-1]
+
+    coexpression_norm += coexpression_norm.T
+    np.fill_diagonal(coexpression_norm, 0)
+
+
+    plt.imshow(coexpression_norm, cmap='Oranges')
+    plt.show()
+
+    distances = np.mean([bin_edges[:-1], bin_edges[1:]], axis=0)
+    return distances, values
+
+
+
+
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset', type=str, default='GM19238')
+    parser.add_argument('--dataset', type=str, default='MCF7')
     parser.add_argument('--chr', type=int, default=2)
     parser.add_argument('--type', type=str, choices=['observed', 'oe'], default='oe')
     parser.add_argument('--norm', type=str, choices=['NONE', 'VC', 'VC_SQRT', 'KR'], default='NONE')
     parser.add_argument('--file', type=str, choices=['primary', 'replicate', 'combined'], default='combined')
+    parser.add_argument('--data-type', type=str, choices=['coexp', 'interactions'], default='coexp')
     parser.add_argument('--resolution', type=int, default=10000)
     parser.add_argument('--save-coexp', default=False, action='store_true')
     parser.add_argument('--save-interactions', default=False, action='store_true')
     parser.add_argument('--save-hic', default=False, action='store_true')
+    parser.add_argument('--real-distance', default=True, action='store_true')
     args = parser.parse_args()
+
+
 
     data_folder = 'data/{}/'.format(args.dataset)
     hic_folder = data_folder + 'hic/'
     rna_folder = data_folder + 'rna/'
     hic_file = '{}_{}_{}_{}_{}_{}'.format(args.file, args.type, args.norm, args.chr, args.chr,
                                           args.resolution)
+    if args.data_type == 'coexp':
+        coexpression = np.load(
+            '/home/varrone/Prj/gene-expression-chromatin/src/coexp_hic_corr/data/{}/coexp/coexpression_chr_{:02d}_{:02d}.npy'.format(
+                args.dataset, args.chr, args.chr))
+        np.fill_diagonal(coexpression, 0)
 
-    expression = np.load(rna_folder + str(args.dataset) + '_chr_{:02d}_zero_median.npy'.format(args.chr))
-    coexpression = np.corrcoef(expression)
-
-    plt.plot(*get_values_from_distance(coexpression))
-    plt.ylabel('Avg. coexpression (Pearson)')
-    plt.xlabel('Gene distance')
-    plt.title('Chromosome {}'.format(args.chr))
-    if args.save_coexp:
-        plt.savefig('plots/coexp_distance_chr_{:02d}.png'.format(args.chr))
+        if args.real_distance:
+            plt.plot(*get_values_from_real_distance(args.dataset, args.chr, coexpression))
+        else:
+            plt.plot(*get_values_from_distance(coexpression))
+        plt.ylabel('Avg. coexpression (Pearson)')
+        plt.xlabel('Gene distance')
+        plt.title('Chromosome {}'.format(args.chr))
+        if args.save_coexp:
+            plt.savefig('plots/coexp_distance_chr_{:02d}.png'.format(args.chr))
+        else:
+            plt.show()
+        plt.clf()
     else:
-        plt.show()
-    plt.clf()
+        contact_matrix = np.load(hic_folder + hic_file + '.npy')
+        contact_matrix = np.triu(contact_matrix, 1)
 
-    contact_matrix = np.load(hic_folder + hic_file + '.npy')
-    contact_matrix = np.triu(contact_matrix, 1)
-
-    plt.plot(*get_values_from_distance(contact_matrix))
-    plt.ylabel('Avg. normalized number of interactions')
-    plt.xlabel('Gene distance')
-    plt.title('Chromosome {}'.format(args.chr))
-    if args.save_interactions:
-        plt.savefig('plots/interactions_distance_chr_{:02d}.png'.format(args.chr))
-    else:
-        plt.show()
+        plt.plot(*get_values_from_distance(contact_matrix))
+        plt.ylabel('Avg. normalized number of interactions')
+        plt.xlabel('Gene distance')
+        plt.title('Chromosome {}'.format(args.chr))
+        if args.save_interactions:
+            plt.savefig('plots/interactions_distance_chr_{:02d}.png'.format(args.chr))
+        else:
+            plt.show()
 

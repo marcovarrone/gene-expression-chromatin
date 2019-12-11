89cb6b4c16f9debfaf5956b56b63db852ed1226f
Marco Varrone <mrc.varrone@gmail.com>
2019-12-11 18:02:24 +0100

    Hi-C generation

--- src/coexp_hic_corr/02_hic_plot_juicer.py
+++ src/coexp_hic_corr/02_hic_plot_juicer.py
@@ -12,13 +12,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-rd', '--rna-dataset', type=str, default='MCF7')
 parser.add_argument('-hd', '--hic-dataset', type=str, default='MCF7')
 parser.add_argument('--type', type=str, choices=['observed', 'oe'], default='observed')
-parser.add_argument('--norm', type=str, choices=['NONE', 'VC', 'VC_SQRT', 'KR'], default='KR')
+parser.add_argument('--norm', type=str, choices=['NONE', 'VC', 'VC_SQRT', 'KR'], default='NONE')
 parser.add_argument('--file', type=str, choices=['primary', 'replicate', 'combined'], default='primary')
-parser.add_argument('--chr-src', type=int, default=2)
+parser.add_argument('--chr-src', type=int, default=1)
 parser.add_argument('--chr-tgt', type=int, default=2)
-parser.add_argument('--resolution', type=int, default=10000)
-parser.add_argument('--window', type=int, default=80000)
-parser.add_argument('--aggregation', type=str, choices=['median', 'sum', 'max'], default='sum')
+parser.add_argument('--resolution', type=int, default=50000)
+parser.add_argument('--window', type=int, default=50000)
+parser.add_argument('--aggregation', type=str, choices=['median', 'sum', 'max', None], default='sum')
 parser.add_argument('--save-matrix', default=False, action='store_true')
 parser.add_argument('--csv', default=False, action='store_true')
 parser.add_argument('--save-plot', default=False, action='store_true')
@@ -34,7 +34,8 @@ output_file = '{}_{}_{}_{}'.format(args.file, args.chr_src, args.chr_tgt, args.r
 sps_path = data_folder + hic_folder + output_file + '.npz'
 
 
-def get_gene_bins(gene, bin_size, window):
+# ToDo: fix binning using digitize
+def get_gene_bins(gene, bin_size, window, bins):
     tss = gene['Transcription start site (TSS)']
     tss_bin = round(tss / bin_size)
     start = tss_bin - int(np.ceil((window/2) / bin_size))
@@ -44,19 +45,30 @@ def get_gene_bins(gene, bin_size, window):
 
 def generate_hic(hic, gene_info_src, gene_info_tgt, resolution, window):
     contact_matrix = np.zeros((gene_info_src.shape[0], gene_info_tgt.shape[0]))
+
+    bins = np.arange(0, np.max(np.concatenate(gene_info_src['Transcription start site (TSS)'], gene_info_tgt['Transcription start site (TSS)'])+window, window))
     for i, (idx1, gene1) in enumerate(gene_info_src.iterrows()):
-        start1, end1 = get_gene_bins(gene1, resolution, window)
+        start1, end1 = get_gene_bins(gene1, resolution, window, bins)
         print("Processing gene", i, "/", gene_info_src.shape[0])
 
         for j, (idx2, gene2) in enumerate(gene_info_tgt.iterrows()):
-            start2, end2 = get_gene_bins(gene2, resolution, window)
+            start2, end2 = get_gene_bins(gene2, resolution, window, bins)
+            if args.window == 0:
+                end1 += 1
+                end2 += 1
+
             mat = hic[start1:end1, start2:end2].A
             if args.aggregation == 'median':
                 value = np.median(np.median(mat))
             elif args.aggregation == 'max':
                 value = np.max(mat)
-            else:
+            elif args.aggregation == 'sum':
                 value = np.sum(mat)
+            else:
+                if args.window == 0:
+                    value = mat[0]
+                else:
+                    raise ValueError
             contact_matrix[i, j] += value
             if gene1['Chromosome/scaffold name'] == gene2['Chromosome/scaffold name']:
                 contact_matrix[j, i] += value
@@ -73,9 +85,9 @@ if __name__ == '__main__':
     if not os.path.exists(rna_folder):
         os.makedirs(rna_folder)
 
-    hic_output_file = '{}_{}_{}_{}_{}_{}_{}{}_{}'.format(args.file, args.type, args.norm, args.chr_src, args.chr_tgt,
+    hic_output_file = '{}_{}_{}_{}_{}_{}_{}{}{}'.format(args.file, args.type, args.norm, args.chr_src, args.chr_tgt,
                                                      args.resolution, args.window, '_all' if args.all_regions else '',
-                                                     args.aggregation)
+                                                        ('_' + args.aggregation) if args.aggregation else '')
 
     print(hic_output_file)
 
@@ -88,13 +100,19 @@ if __name__ == '__main__':
     if os.path.exists(hic_folder + hic_output_file + '.npy') and not args.force:
         contact_matrix = np.load(hic_folder + hic_output_file + '.npy')
     else:
+        downloaded = False
         if not os.path.exists(sps_path):
+            downloaded = True
             print("Hi-C data not present. Downloading...")
             os.system(
                 'python3 01_juicer2numpy.py --dataset {} --type {} --norm {} --file {} --chr-src {} --chr-tgt {} --resolution {}'.format(
                     args.hic_dataset, args.type, args.norm, args.file, args.chr_src, args.chr_tgt, args.resolution))
             print("Downloading completed")
         hic_matrix = sps.load_npz(sps_path)
+
+        #if downloaded:
+        #    os.remove(sps_path)
+
         if args.all_regions:
             contact_matrix = hic_matrix.A
             contact_matrix[np.isnan(contact_matrix)] = 0
@@ -126,9 +144,9 @@ if __name__ == '__main__':
         else:
             np.save(hic_folder + hic_output_file + '.npy', contact_matrix)
 
-    plt.imshow(np.rot90(np.log2(contact_matrix + 1)), cmap="Oranges")
+    plt.imshow(np.log2(contact_matrix + 1), cmap="Oranges")
     if args.save_plot:
         if not os.path.exists('plots'):
             os.makedirs('plots')
-        plt.savefig('plots/{}.png'.format(hic_output_file))
+        plt.savefig('plots/{}/{}.png'.format(args.hic_dataset, hic_output_file))
     plt.show()

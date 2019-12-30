3bfa75ab0bab106f1e02550cf83c2c31f7706313
Marco Varrone <mrc.varrone@gmail.com>
2019-12-30 23:29:03 +0100

    Unify part of the embedding methods' code

--- src/link_prediction/03_node2vec.py
+++ src/link_prediction/03_node2vec.py
@@ -1,77 +1,60 @@
 import argparse
-import os
 
-import networkx as nx
-import numpy as np
-import scipy.sparse as sps
-from bionev.utils import load_embedding
-
-from link_prediction.utils import set_n_threads
+from link_prediction.utils import set_n_threads, generate_embedding
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', type=str, default='MCF7')
-    parser.add_argument('--interactions', type=str, default='primary_oe_NONE_1_1_10000_40000_sum_0.0')
+    parser.add_argument('--interactions', type=str,
+                        default=None,
+                        required=True)
+    parser.add_argument('--coexpression', type=str, default=None)
     parser.add_argument('--emb-size', type=int, default=8)
     parser.add_argument('--n-jobs', type=int, default=10)
     parser.add_argument('--num-walks', type=int, default=10)
     parser.add_argument('--walk-len', type=int, default=80)
     parser.add_argument('--p', type=float, default=1.0)
     parser.add_argument('--q', type=float, default=1.0)
+    parser.add_argument('--window', type=int, default=10)
+    parser.add_argument('--threshold', type=float, default=None)
 
     parser.add_argument('--all-regions', default=False, action='store_true')
-    parser.add_argument('--force', default=False, action='store_true')
+    parser.add_argument('--force', default=True, action='store_true')
     parser.add_argument('--method', default='node2vec', type=str, choices=['node2vec', 'struc2vec'])
     parser.add_argument('--weighted', type=str, default='False')
     parser.add_argument('--save-emb', default=False, action='store_true')
+    parser.add_argument('--task', type=str, default='none')
+    parser.add_argument('--genes-chr', default=False, action='store_true')
+
     args = parser.parse_args()
 
     set_n_threads(args.n_jobs)
 
-    interactions_path = './data/{}/interactions/interactions_{}.npy'.format(
-        args.dataset, args.interactions)
+    if args.coexpression:
+        args.folder = 'coexpression'
+        args.name = args.coexpression
+    else:
+        args.folder = 'interactions'
+        args.name = args.interactions
 
-    emb_path = '{}_es{}_nw{}_wl{}_p{}_q{}_prova'.format(args.interactions, args.emb_size, args.num_walks, args.walk_len, args.p, args.q)
-    if not os.path.exists('embeddings/{}/{}/{}.npy'.format(args.dataset, args.method, emb_path)) or args.force:
-        adj = np.load(interactions_path)
-        if adj.shape[0] != adj.shape[1]:
-            # adj = np.block([[np.zeros((adj.shape[0], adj.shape[0])), adj], [adj.T, np.zeros((adj.shape[1], adj.shape[1]))]])
-            graph = nx.algorithms.bipartite.from_biadjacency_matrix(sps.csr_matrix(adj))
-        else:
-            graph = nx.from_numpy_array(adj)
+    # ToDo: handle better the adding of the threshold term in the interactions path
+    # (and its absence in gene_chr path)
+    interactions_path = './data/{}/{}/{}_{}{}.npy'.format(
+        args.dataset, args.folder, args.folder, args.name, '_' + str(args.threshold) if args.threshold else '')
 
-        if args.weighted == 'True':
-            nx.write_weighted_edgelist(graph,
-                                       'data/{}/interactions/{}.edgelist'.format(args.dataset, args.interactions))
-        else:
-            nx.write_edgelist(graph, 'data/{}/interactions/{}.edgelist'.format(args.dataset, args.interactions))
+    emb_path = '{}{}_es{}_nw{}_wl{}_p{}_q{}'.format(args.name,
+                                                    '_' + str(args.threshold) if args.threshold else '', args.emb_size,
+                                                    args.num_walks, args.walk_len, args.p, args.q)
 
-        command = 'bionev --input data/{}/interactions/{}.edgelist '.format(args.dataset, args.interactions) + \
-                  '--output ./embeddings/{}/{}/{}.txt '.format(args.dataset, args.method.lower(), emb_path) + \
-                  '--method {} --task link-prediction '.format(args.method) + \
-                  '--dimensions {} '.format(args.emb_size) + \
-                  ('--weighted True' if args.weighted == 'True' else '')
-                  #'--number-walks {} --walk-length {} --window-size 10 '.format(args.num_walks, args.walk_len) + \
-                  #'--p {} --q {} '.format(args.p, args.q) + \
+    command = 'bionev --input data/{}/{}/{}{}.edgelist '.format(args.dataset, args.folder, args.name, '_' + str(
+        args.threshold) if args.threshold else '') + \
+              '--output ./embeddings/{}/{}/{}.txt '.format(args.dataset, args.method.lower(), emb_path) + \
+              '--method {} --task {} '.format(args.method, args.task) + \
+              '--dimensions {} '.format(args.emb_size) + \
+              '--number-walks {} '.format(args.num_walks) + \
+              '--walk-length {} '.format(args.walk_len) + \
+              '--p {} --q {} '.format(args.p, args.q) + \
+              '--window-size {} '.format(args.window) + \
+              ('--weighted True' if args.weighted == 'True' else '')
 
-        print(command)
-        os.system(command)
-        emb_dict = load_embedding('./embeddings/{}/{}/{}.txt'.format(args.dataset, args.method.lower(), emb_path))
-
-        if args.save_emb:
-            emb_dict = load_embedding('./embeddings/{}/{}/{}.txt'.format(args.dataset, args.method.lower(), emb_path))
-            emb = np.zeros((len(emb_dict.keys()), args.emb_size))
-            for i in range(emb.shape[0]):
-                emb[i, :] = emb_dict[str(i)]
-
-            if args.all_regions:
-                np.save('./embeddings/{}/{}/{}_full.npy'.format(args.dataset, args.method.lower(), emb_path), emb)
-                # ToDo: better removal of _90 in interactions
-                gene_idxs = np.load(
-                    '/home/varrone/Prj/gene-expression-chromatin/src/coexp_hic_corr/data/{}/gene_idxs/{}.npy'.format(
-                        args.dataset, args.interactions[:-3]))
-                emb = emb[gene_idxs]
-
-            np.save('./embeddings/{}/{}/{}_prova.npy'.format(args.dataset, args.method.lower(), emb_path), emb)
-        os.remove('./embeddings/{}/{}/{}.txt'.format(args.dataset, args.method.lower(), emb_path))
-        os.remove('data/{}/interactions/{}.edgelist'.format(args.dataset, args.interactions))
+    generate_embedding(args, emb_path, interactions_path, command)
--- src/link_prediction/utils.py
+++ src/link_prediction/utils.py
@@ -1,23 +1,27 @@
 import os
+from time import time
 
+import networkx as nx
+import numpy as np
+import scipy.sparse as sps
+from bionev.utils import load_embedding
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score, f1_score, precision_score, recall_score
 from sklearn.model_selection import StratifiedKFold
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
-import matplotlib.pyplot as plt
-import numpy as np
 
 
-def evaluate_embedding(X_train, y_train, classifier_name, n_iter=10, seed=42, verbose=1):
+def evaluate_embedding(X_train, y_train, classifier_name, n_iter=10, seed=42, verbose=1, clf_params={}):
     rocs = []
     accuracies = []
     f1s = []
     precisions = []
     recalls = []
     for i in range(n_iter):
-        skf = StratifiedKFold(n_splits=10,shuffle=True)
+        skf = StratifiedKFold(n_splits=10, shuffle=True)
         for train_index, val_index in skf.split(X_train, y_train):
             if classifier_name == 'mlp':
                 classifier = MLPClassifier(max_iter=500, hidden_layer_sizes=(800,))
@@ -25,6 +29,8 @@ def evaluate_embedding(X_train, y_train, classifier_name, n_iter=10, seed=42, ve
                 classifier = SVC(gamma='scale')
             elif classifier_name == 'mlp_2':
                 classifier = MLPClassifier(max_iter=500, hidden_layer_sizes=(100, 100,), random_state=seed)
+            elif classifier_name == 'rf':
+                classifier = RandomForestClassifier(n_jobs=10, **clf_params)
             else:
                 classifier = LogisticRegression(max_iter=500, solver='lbfgs')
 
@@ -32,8 +38,11 @@ def evaluate_embedding(X_train, y_train, classifier_name, n_iter=10, seed=42, ve
             X_train_scaled = scaler.fit_transform(X_train[train_index], y_train[train_index])
             X_valid_scaled = scaler.transform(X_train[val_index])
 
+            start = time()
             classifier.fit(X_train_scaled, y_train[train_index])
             y_pred = classifier.predict(X_valid_scaled)
+            end = time()
+            print('Running time:', end - start)
 
             roc = roc_auc_score(y_train[val_index], y_pred)
             accuracy = classifier.score(X_valid_scaled, y_train[val_index])
@@ -51,10 +60,63 @@ def evaluate_embedding(X_train, y_train, classifier_name, n_iter=10, seed=42, ve
     return {'acc': accuracies, 'roc': rocs, 'f1': f1s, 'precision': precisions, 'recall': recalls}
 
 
+def generate_embedding(args, emb_path, interactions_path, command):
+    if not os.path.exists('embeddings/{}/{}/{}.npy'.format(args.dataset, args.method, emb_path)) or args.force:
+        adj = np.load(interactions_path)
+        adj[np.isnan(adj)] = 0
+        if adj.shape[0] != adj.shape[1]:
+            # adj = np.block([[np.zeros((adj.shape[0], adj.shape[0])), adj], [adj.T, np.zeros((adj.shape[1], adj.shape[1]))]])
+            graph = nx.algorithms.bipartite.from_biadjacency_matrix(sps.csr_matrix(adj))
+        else:
+            graph = nx.from_numpy_array(adj)
+
+        if args.weighted == 'True':
+            nx.write_weighted_edgelist(graph,
+                                       'data/{}/{}/{}{}.edgelist'.format(args.dataset, args.folder, args.name,
+                                                                         '_' + str(
+                                                                             args.threshold) if args.threshold else ''))
+        else:
+            nx.write_edgelist(graph, 'data/{}/{}/{}{}.edgelist'.format(args.dataset, args.folder, args.name,
+                                                                       '_' + str(
+                                                                           args.threshold) if args.threshold else ''),
+                              data=False)
+
+        print(command)
+        os.system(command)
+        emb_dict = load_embedding(
+            '/home/varrone/Prj/gene-expression-chromatin/src/link_prediction/embeddings/{}/{}/{}.txt'.format(
+                args.dataset, args.method.lower(), emb_path))
+
+        emb = np.zeros((adj.shape[0], args.emb_size))
+
+        disconnected_nodes = []
+
+        print('N. genes', adj.shape[0])
+        for gene in range(adj.shape[0]):
+            try:
+                emb[gene, :] = emb_dict[str(gene)]
+            except KeyError:
+                print('KeyError for', gene)
+                # np.delete(emb, i, axis=0)
+                emb[gene, :] = np.zeros(args.emb_size)
+                disconnected_nodes.append(gene)
+
+        np.save(
+            '/home/varrone/Prj/gene-expression-chromatin/src/coexp_hic_corr/data/{}/disconnected_nodes/{}.npy'.format(
+                args.dataset, args.name), np.array(disconnected_nodes))
+
+        if args.save_emb:
+            np.save('./embeddings/{}/{}/{}.npy'.format(args.dataset, args.method.lower(), emb_path), emb)
+        os.remove('./embeddings/{}/{}/{}.txt'.format(args.dataset, args.method.lower(), emb_path))
+        os.remove('data/{}/interactions/{}.edgelist'.format(args.dataset, args.interactions))
+        return emb
+
+
 def set_gpu(active=False):
     if not active:
         os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
         os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
 
 def set_n_threads(n_threads):
     os.environ["OMP_NUM_THREADS"] = str(n_threads)

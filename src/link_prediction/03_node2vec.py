07a62f3a360a23f29e70b74bf6268d1911f05e50
Marco Varrone <mrc.varrone@gmail.com>
2020-01-16 12:26:37 +0100

    Update node2vec

--- src/link_prediction/03_node2vec.py
+++ src/link_prediction/03_node2vec.py
@@ -6,8 +6,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', type=str, default='MCF7')
     parser.add_argument('--interactions', type=str,
-                        default=None,
-                        required=True)
+                        default=None)
     parser.add_argument('--coexpression', type=str, default=None)
     parser.add_argument('--emb-size', type=int, default=8)
     parser.add_argument('--n-jobs', type=int, default=10)
@@ -16,15 +15,13 @@ if __name__ == '__main__':
     parser.add_argument('--p', type=float, default=1.0)
     parser.add_argument('--q', type=float, default=1.0)
     parser.add_argument('--window', type=int, default=10)
-    parser.add_argument('--threshold', type=float, default=None)
 
     parser.add_argument('--all-regions', default=False, action='store_true')
-    parser.add_argument('--force', default=True, action='store_true')
+    parser.add_argument('--force', default=False, action='store_true')
     parser.add_argument('--method', default='node2vec', type=str, choices=['node2vec', 'struc2vec'])
     parser.add_argument('--weighted', type=str, default='False')
     parser.add_argument('--save-emb', default=False, action='store_true')
-    parser.add_argument('--task', type=str, default='none')
-    parser.add_argument('--genes-chr', default=False, action='store_true')
+    parser.add_argument('--task', type=str, default='none', choices=['none', 'link-prediction'])
 
     args = parser.parse_args()
 
@@ -37,17 +34,13 @@ if __name__ == '__main__':
         args.folder = 'interactions'
         args.name = args.interactions
 
-    # ToDo: handle better the adding of the threshold term in the interactions path
-    # (and its absence in gene_chr path)
-    interactions_path = './data/{}/{}/{}_{}{}.npy'.format(
-        args.dataset, args.folder, args.folder, args.name, '_' + str(args.threshold) if args.threshold else '')
+    interactions_path = './data/{}/{}/{}_{}.npz'.format(
+        args.dataset, args.folder, args.folder, args.name)
 
-    emb_path = '{}{}_es{}_nw{}_wl{}_p{}_q{}'.format(args.name,
-                                                    '_' + str(args.threshold) if args.threshold else '', args.emb_size,
+    emb_path = '{}_es{}_nw{}_wl{}_p{}_q{}'.format(args.name, args.emb_size,
                                                     args.num_walks, args.walk_len, args.p, args.q)
 
-    command = 'bionev --input data/{}/{}/{}{}.edgelist '.format(args.dataset, args.folder, args.name, '_' + str(
-        args.threshold) if args.threshold else '') + \
+    command = 'bionev --input data/{}/{}/{}.edgelist '.format(args.dataset, args.folder, args.name) + \
               '--output ./embeddings/{}/{}/{}.txt '.format(args.dataset, args.method.lower(), emb_path) + \
               '--method {} --task {} '.format(args.method, args.task) + \
               '--dimensions {} '.format(args.emb_size) + \

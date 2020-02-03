c1f145d629272bf6b8a11d45e5230ed70a27c575
Marco Varrone <mrc.varrone@gmail.com>
2020-02-03 23:02:22 +0100

    Refactoring coexpression preprocessing

--- src/01_preprocessing/01_coexpression.py
+++ src/01_preprocessing/01_coexpression.py
@@ -1,29 +1,51 @@
+import argparse
+import os
+
 import pandas as pd
 
-if __name__ == '__main__':
 
-    tcga = pd.read_csv('/home/varrone/Data/PRAD/HiSeqV2', delimiter='\t')
-    print(tcga.shape)
-    tcga = tcga[(tcga == 0).sum(axis=1) <= tcga.shape[1] * 0.2]
-    print(tcga.shape)
+def main(args):
+    tcga = pd.read_csv(args.input, delimiter='\t')
 
-    gene_info = pd.read_csv('/home/varrone/Data/MCF7/GRCh37_p13_gene_info.txt', delimiter='\t')
+    print('Gene expression data loaded:', tcga.shape[0], 'genes and', tcga.shape[1], 'samples')
 
+    low_expression_genes = (tcga == 0).sum(axis=1) <= tcga.shape[1] * 0.2
+    tcga = tcga[low_expression_genes]
+    print(tcga.shape[0] - low_expression_genes.sum(), 'genes out of', tcga.shape[0],
+          'have more that 20% of samples with 0 expression. Removed')
 
-    tcga = gene_info.merge(tcga, right_on='sample', left_on='Gene name',)
+    gene_info = pd.read_csv('../../data/GRCh37_p13_gene_info.txt', delimiter='\t')
+
+    print('Merging gene expression data with gene information from Ensembl hg19')
+    tcga = gene_info.merge(tcga, right_on='sample', left_on='Gene name', )
     tcga = tcga.drop('sample', axis=1)
+
+    print('Removing duplicated gene entries and keeping the ones with the outermost TSS')
     tcga_pos = tcga[tcga['Strand'] == 1]
     tcga_neg = tcga[tcga['Strand'] == -1]
     tcga_pos = tcga_pos.groupby(['Gene name']).min()
     tcga_neg = tcga_neg.groupby(['Gene name']).max()
 
-
     tcga = pd.concat([tcga_neg, tcga_pos])
-    print(tcga.shape)
     tcga = tcga.groupby(['Gene name']).max()
-    print(tcga.shape)
 
-    #scaler = StandardScaler()
-    #tcga_exp_scaled = scaler.fit_transform(tcga_exp)
-    #tcga.to_csv('/home/varrone/Data/PRAD/TCGA_PRAD_GRCh37_p13.csv')
-    pass
+    print('Final gene expression data with', tcga.shape[0], 'genes')
+
+    dataset_path = '../../data/{}'.format(args.dataset)
+    if not os.path.exists(dataset_path):
+        os.mkdir(dataset_path)
+
+    print('Saving in', dataset_path + '/coexpression_raw.csv')
+    tcga.to_csv(dataset_path + '/coexpression_raw.csv')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--input', type=str, required=True,
+                        help='Gene expression input file path downloaded from Xena Browser')
+    parser.add_argument('--dataset', type=str, required=True,
+                        help='Name that will be used to identify the dataset')
+    args = parser.parse_args()
+
+    main(args)

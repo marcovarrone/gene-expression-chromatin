1b46d38b2e7485882cecd05b6190aa19ece8fe11
Marco Varrone <mrc.varrone@gmail.com>
2020-02-19 09:36:46 +0100

    Set Hi-C resolution before selecting genes

--- src/preprocessing/02_hic_prostate.py
+++ src/preprocessing/02_hic_prostate.py
@@ -9,6 +9,7 @@ import scipy.sparse as sps
 def main(args):
     print('Loading bin coordinates')
     coords = pd.read_csv(args.bed_path, delimiter='\t', header=None)
+
     coords[1] = (coords[1] // args.resolution).astype(int)
     coords[2] = (coords[2] // args.resolution).astype(int)
 
@@ -27,21 +28,26 @@ def main(args):
         print('Chromosome ', chr_source)
         for chr_target in chromosomes[i:]:
             if os.path.exists(dataset_path + '/primary_observed_ICE_{}_{}_{}.npz'.format(chr_source, chr_target,
-                                                                               args.resolution)):
+                                                                                         args.window)):
+                print('File already existing. Skip.')
                 continue
             coords_chr_src = coords[coords[0] == 'chr{}'.format(chr_source)]
             coords_chr_tgt = coords[coords[0] == 'chr{}'.format(chr_target)]
-
             min_src = np.min(coords_chr_src[3])
             max_src = np.max(coords_chr_src[3])
 
             min_tgt = np.min(coords_chr_tgt[3])
             max_tgt = np.max(coords_chr_tgt[3])
             values_chr = values[min_src:max_src, min_tgt:max_tgt]
+            values_chr = values_chr.toarray()
+            values_chr = np.add.reduceat(values_chr, np.arange(0, values_chr.shape[0], args.window // args.resolution),
+                                         axis=0)
+            values_chr = np.add.reduceat(values_chr, np.arange(0, values_chr.shape[1], args.window // args.resolution),
+                                         axis=1)
             sps.save_npz(
-                dataset_path + '/primary_observed_ICE_{}_{}_{}.npz'.format(chr_source, chr_target, args.resolution),
-                values_chr)
-    print('Hi-C data saved in sparse format in', dataset_path)
+                dataset_path + '/primary_observed_ICE_{}_{}_{}.npz'.format(chr_source, chr_target, args.window),
+                sps.csr_matrix(values_chr))
+            print('Hi-C data saved in sparse format in', dataset_path + '/primary_observed_ICE_{}_{}_{}.npz'.format(chr_source, chr_target, args.window))
 
 
 if __name__ == '__main__':
@@ -53,10 +59,16 @@ if __name__ == '__main__':
                         help='Path of the original Hi-C matrix')
     parser.add_argument('--resolution', type=int, required=True,
                         help='Resolution of the Hi-C data.')
+    parser.add_argument('--window', type=int, required=True,
+                        help='Resolution of the Hi-C data.')
     parser.add_argument('--chromosomes', nargs='*', default=None,
                         help='List of chromosomes for which to extract the Hi-C data. If empty all the non-sexual chromosomes data will be extracted.')
     parser.add_argument('--inter', default=False, action='store_true',
                         help='Extract also interchromosomal interactions')
+
     args = parser.parse_args()
+
+    if args.window % args.resolution != 0:
+        raise ValueError('window must be a multiple of the resolution')
 
     main(args)

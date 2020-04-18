d5fb0bf5b30ed297673b29118b4827434a612672
Marco Varrone <mrc.varrone@gmail.com>
2020-04-18 10:05:11 +0200

    Refactoring coexpression network building

--- src/network_building/02_coexpression_network.py
+++ src/network_building/02_coexpression_network.py
@@ -11,29 +11,34 @@ def single_chromosome(args):
     data_folder = '../../data/{}/coexpression_networks'.format(args.dataset)
     os.makedirs(data_folder, exist_ok=True)
 
-    coexpression = np.load(
-        '../../data/{}/coexpression/coexpression_chr_{}_{}.npy'.format(args.dataset, args.chr_src, args.chr_tgt))
+    filename_plot = '../../plots/{}/coexpression_networks/coexpression_chr_{}_{}_{}{}.png'.format(
+        args.dataset, args.chr_src,
+        args.chr_tgt,
+        args.perc_intra,
+        '_abs' if args.abs else '')
 
-    print('Computing co-expression network between chromosome', args.chr_src, 'and chromosome', args.chr_tgt)
+    if not os.path.exists(
+            data_folder + '/coexpression_chr_{}_{}_{}{}.npy'.format(args.chr_src, args.chr_tgt, args.perc_intra,
+                                                                    '_abs' if args.abs else '')):
+        coexpression = np.load(
+            '../../data/{}/coexpression/coexpression_chr_{}_{}.npy'.format(args.dataset, args.chr_src, args.chr_tgt))
 
-    if args.abs:
-        coexpression = np.abs(coexpression)
+        print('Computing co-expression network between chromosome', args.chr_src, 'and chromosome', args.chr_tgt)
 
-    is_intra = (args.chr_src == args.chr_tgt)
-    if is_intra and args.perc_intra is not None:
-        threshold = coexpression_threshold(args.dataset, percentile_intra=args.perc_intra)
-    elif not is_intra and args.perc_inter is not None:
-        threshold = coexpression_threshold(args.dataset, percentile_inter=args.perc_inter)
-    elif args.threshold:
-        threshold = args.threshold
-    else:
-        raise ValueError('Either one parameter between --percentile and --threshold must be passed.')
+        if args.abs:
+            coexpression = np.abs(coexpression)
 
-    threshold = np.round(threshold, 2)
+        # ToDo: implement that if single chromosome computes the threshold not shared
+        # ToDo: remove or improve implementation for generating inter-chromosomal coexpression
+        is_intra = (args.chr_src == args.chr_tgt)
+        if is_intra and args.perc_intra is not None:
+            threshold = coexpression_threshold(args.dataset, percentile_intra=args.perc_intra)
+        elif not is_intra and args.perc_inter is not None:
+            threshold = coexpression_threshold(args.dataset, percentile_inter=args.perc_inter)
+        else:
+            raise ValueError('Either one parameter between --percentile and --threshold must be passed.')
 
-    if not os.path.exists(data_folder + '/coexpression_chr_{}_{}_{}{}.npy'.format(args.chr_src, args.chr_tgt, threshold,
-                                                                                  '_abs' if args.abs else '')):
-
+        threshold = np.round(threshold, 2)
         print('Treshold:', threshold)
 
         coexpression[coexpression < threshold] = 0
@@ -45,71 +50,69 @@ def single_chromosome(args):
             np.save(
                 data_folder + '/coexpression_chr_{}_{}_{}{}.npy'.format(args.chr_src,
                                                                         args.chr_tgt,
-                                                                        threshold,
+                                                                        args.perc_intra,
                                                                         '_abs' if args.abs else ''),
                 coexpression)
     else:
-        coexpression = np.load(data_folder + '/coexpression_chr_{}_{}_{}{}.npy'.format(args.chr_src, args.chr_tgt,
-                                                                                       threshold,
-                                                                                       '_abs' if args.abs else ''))
-
-    if args.save_plot:
+        print('Coexpression network for chromosome', args.chr_src, 'already computed. Skipped')
+        if args.save_plot and not os.path.exists(filename_plot):
+            coexpression = np.load(data_folder + '/coexpression_chr_{}_{}_{}{}.npy'.format(args.chr_src, args.chr_tgt,
+                                                                                           args.perc_intra,
+                                                                                           '_abs' if args.abs else ''))
+    if args.save_plot and not os.path.exists(filename_plot):
         os.makedirs('../../plots/{}/coexpression_networks/'.format(args.dataset), exist_ok=True)
         plt.imshow(coexpression, cmap='Oranges')
-        plt.savefig(
-            '../../plots/{}/coexpression_networks/coexpression_chr_{}_{}_{}{}.png'.format(args.dataset, args.chr_src,
-                                                                                          args.chr_tgt,
-                                                                                          threshold,
-                                                                                          '_abs' if args.abs else ''))
+        plt.savefig(filename_plot)
         plt.close()
 
 
 def multi_chromosome(args):
-    coexpression_full = np.load('../../data/{}/coexpression/coexpression_chr_all_all.npy'.format(args.dataset))
-
-    shapes = [np.load('../../data/{}/coexpression/coexpression_chr_{}_{}.npy'.format(args.dataset, i, i)).shape for i in
-              range(1, 23)]
-
-    mask = intra_mask(shapes)
-
+    data_folder = '../../data/{}/coexpression_networks'.format(args.dataset)
     if args.perc_inter is not None:
-        threshold_inter = coexpression_threshold(args.dataset, percentile_inter=args.perc_inter)
-        coexpression_inter = coexpression_full * np.logical_not(mask)
-        coexpression_inter[coexpression_inter < threshold_inter] = 0
-        coexpression_inter[coexpression_inter > 0] = 1
+        filename = 'coexpression_chr_all_{}_{}'.format(args.perc_intra, args.perc_inter)
     else:
-        coexpression_inter = np.ones(coexpression_full.shape)
-        coexpression_inter *= np.logical_not(mask)
-        coexpression_inter[coexpression_inter == 1] = np.nan
+        filename = 'coexpression_chr_all_{}'.format(args.perc_intra)
 
-    threshold_intra = coexpression_threshold(args.dataset, percentile_intra=args.perc_intra)
-    coexpression_intra = coexpression_full * mask
-    coexpression_intra[coexpression_intra < threshold_intra] = 0
-    coexpression_intra[coexpression_intra > 0] = 1
+    if not os.path.exists(data_folder + '/' + filename + '.npy'.format(filename)):
+        coexpression_full = np.load('../../data/{}/coexpression/coexpression_chr_all_all.npy'.format(args.dataset))
 
-    print('N. intra-chromosomal interactions:', np.nansum(coexpression_intra))
-    print('N. inter-chromosomal interactions:', np.nansum(coexpression_inter))
+        shapes = [np.load('../../data/{}/coexpression/coexpression_chr_{}_{}.npy'.format(args.dataset, i, i)).shape for
+                  i in
+                  range(1, 23)]
 
-    coexpression_thr = coexpression_intra + coexpression_inter
+        mask = intra_mask(shapes)
 
-    if args.perc_inter is not None:
-        filename = 'coexpression_chr_all_{}_{}'.format(threshold_intra, threshold_inter)
-    else:
-        filename = 'coexpression_chr_all_{}'.format(threshold_intra)
+        if args.perc_inter is not None:
+            threshold_inter = coexpression_threshold(args.dataset, percentile_inter=args.perc_inter)
+            coexpression_inter = coexpression_full * np.logical_not(mask)
+            coexpression_inter[coexpression_inter < threshold_inter] = 0
+            coexpression_inter[coexpression_inter > 0] = 1
+        else:
+            coexpression_inter = np.ones(coexpression_full.shape)
+            coexpression_inter *= np.logical_not(mask)
+            coexpression_inter[coexpression_inter == 1] = np.nan
 
-    if args.save_matrix:
-        data_folder = '../../data/{}/coexpression_networks'.format(args.dataset)
-        os.makedirs(data_folder, exist_ok=True)
-        np.save(
-            data_folder + '/' + filename + '.npy'.format(filename), coexpression_thr)
+        threshold_intra = coexpression_threshold(args.dataset, percentile_intra=args.perc_intra)
+        coexpression_intra = coexpression_full * mask
+        coexpression_intra[coexpression_intra < threshold_intra] = 0
+        coexpression_intra[coexpression_intra > 0] = 1
 
-    if args.save_plot:
-        os.makedirs('../../plots/{}/coexpression_networks/'.format(args.dataset), exist_ok=True)
-        plt.figure(figsize=(7,7), dpi=600)
-        plt.imshow(coexpression_thr, cmap='Oranges')
-        plt.savefig(
-            '../../plots/{}/coexpression_networks/{}.png'.format(args.dataset, filename))
-        plt.clf()
+        print('N. intra-chromosomal interactions:', np.nansum(coexpression_intra))
+        print('N. inter-chromosomal interactions:', np.nansum(coexpression_inter))
+
+        coexpression_thr = coexpression_intra + coexpression_inter
+
+        if args.save_matrix:
+            os.makedirs(data_folder, exist_ok=True)
+            np.save(data_folder + '/' + filename + '.npy'.format(filename), coexpression_thr)
+
+        if args.save_plot:
+            os.makedirs('../../plots/{}/coexpression_networks/'.format(args.dataset), exist_ok=True)
+            plt.figure(figsize=(7, 7), dpi=600)
+            plt.imshow(coexpression_thr, cmap='Oranges')
+            plt.savefig(
+                '../../plots/{}/coexpression_networks/{}.png'.format(args.dataset, filename))
+            plt.clf()
 
 
 if __name__ == '__main__':
@@ -117,9 +120,10 @@ if __name__ == '__main__':
     parser.add_argument('--dataset', type=str, default='prostate')
     parser.add_argument('--chr-src', type=int, default=None)
     parser.add_argument('--chr-tgt', type=int, default=None)
-    parser.add_argument('--perc-intra', type=float, default=80)  # , required=True)
-    parser.add_argument('--perc-inter', type=float, default=None)
+    parser.add_argument('--perc-intra', type=float, default=None)  # , required=True)
+    #parser.add_argument('--perc-inter', type=float, default=None)
     parser.add_argument('--single-chrom', default=False, action='store_true')
+    parser.add_argument('--inter', default=False, action='store_true')
     parser.add_argument('--abs', default=False, action='store_true')
     parser.add_argument('--save-plot', default=False, action='store_true')
     parser.add_argument('--save-matrix', default=False, action='store_true')
